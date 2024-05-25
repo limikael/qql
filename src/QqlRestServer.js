d7aabad19b25fbd91460ff7f3b3223ae98b051ad
Mikael Lindqvist <li.mikael@gmail.com>
2024-05-25 18:44:26 +0800

    don't worry about file extension for uploads

--- src/QqlRestServer.js
+++ src/QqlRestServer.js
@@ -21,8 +21,8 @@ export default class QqlRestServer {
                         //console.log("putting: "+data.name+" size: "+data.size);
 
                         let ext=getFileExt(data.name).toLowerCase();
-                        if (!exts.includes(ext))
-                            throw new Error("Unknown file type: "+ext);
+                        /*if (!exts.includes(ext))
+                            throw new Error("Unknown file type: "+ext);*/
 
                         let fn=crypto.randomUUID()+ext;
                         await this.putFile(fn,data);
