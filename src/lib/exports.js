ae9f7f8444fa107bbcbe83c32ec720344c9b4203
Mikael Lindqvist <li.mikael@gmail.com>
2024-12-06 10:03:12 +0800

    fixed exports

--- src/lib/exports.js
+++ src/lib/exports.js
@@ -1,5 +1,6 @@
-export {qqlDriverSqlite, qqlDriverSqlJs, createQql} from "./drivers.js";
-export {default as QqlRestServer} from "./QqlRestServer.js";
-export {default as QqlServer} from "./QqlServer.js";
-export {default as Qql} from "./Qql.js";
-export {createQqlClient} from "./QqlClient.js";
+export {default as QqlDriverSqlite} from "../drivers/QqlDriverSqlite.js";
+export {default as QqlDriverSqlJs} from "../drivers/QqlDriverSqlJs.js";
+export {default as QqlRestServer} from "../net/QqlRestServer.js";
+export {default as QqlServer} from "../net/QqlServer.js";
+export {default as Qql, createQql} from "../qql/Qql.js";
+export {default as QqlClient, createQqlClient} from "../net/QqlClient.js";
